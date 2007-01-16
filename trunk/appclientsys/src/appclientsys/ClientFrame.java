8c7429c1020ff0ac20be960dbc76dc39b74fdc32
prowdev <prowdev@a24a35a2-691c-0410-8238-feb538c7d8c7>
2007-01-16 15:44:36 +0000

    Client plus GUI

--- trunk/appclientsys/src/appclientsys/ClientFrame.java
+++ trunk/appclientsys/src/appclientsys/ClientFrame.java
@@ -13,12 +13,11 @@ package appclientsys;
 
 import appclient.AppClient;
 import appgui.GUIPanel;
+import interfaces.IServer;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.JFrame;
-import javax.swing.JToggleButton;
 import linkclient.*;
-import client.*;
 
 /**
 * The <code> ClientFrame </code> class is a simple interactive
@@ -35,9 +34,12 @@ import client.*;
 * @see ocsftester.SimpleClient
 */
 public class ClientFrame extends JFrame{
-  private InterClient interClient;
+  private AppClient refClient;
+  private IServer interClient;
   private SimpleClient simpleClient;
   private List liste;
+  private String refTitle1, refTitle2;
+  private boolean init;
   
   private Button closeB =     new Button("Close");
   private Button openB =      new Button("Open");
@@ -45,30 +47,32 @@ public class ClientFrame extends JFrame{
   private Button quitB =      new Button("Quit");
   private TextField port =    new TextField("12345");
   private TextField host =    new TextField("localhost");
+  private TextField count =   new TextField("12");
+  private TextField mode =    new TextField("2");  
   private TextField message = new TextField();
   private Label portLB =      new Label("Port: ", Label.RIGHT);
   private Label hostLB =      new Label("Host: ", Label.RIGHT);
+  private Label countLB =     new Label("Clients(<=12):", Label.RIGHT);
+  private Label modeLB =      new Label("Mode(<=3): ", Label.RIGHT);    
   private Label messageLB =   new Label("Message: ", Label.RIGHT);
   private GUIPanel theGUIPanel;
   
-  public ClientFrame(AppClient appClient, InterClient interClient, String title){
+  public ClientFrame(AppClient appClient, InterClient interClient,
+          String title1, String title2){
+    refClient = appClient;
+    refTitle1 = title1;
+    refTitle2 = title2;
     this.interClient = interClient;
     simpleClient = interClient.getSimpleClient();
     liste = interClient.getList();
+    init = true;
+    System.out.println("ClientFrame().init = "+init+
+            " appClient = "+appClient+" theGUIPanel = "+theGUIPanel);            
     
-    int sysMode = appClient.getSysMode();
-    super.setTitle(title+sysMode);
-    int clientCount = appClient.getCount();
-    if (clientCount > 2){
-//        setSize(850,800);
-        setSize(400,600);        
-    }else {
-        setSize(400,800);}
-    
+    setSize(250,600);    //**Initial size before opening network
+    //Overwrite defaults with args if any
     port.setText(String.valueOf(simpleClient.getPort()));
     host.setText(simpleClient.getHost());
-
-    theGUIPanel = new GUIPanel(appClient);
     
     addWindowListener(new WindowAdapter() {
       public void windowClosing(WindowEvent e)
@@ -106,12 +110,17 @@ public class ClientFrame extends JFrame{
     });
 
     Panel bottom = new Panel();
-    bottom.setLayout(new GridLayout(5,2,5,5));
+    //4 rows, 4 columns, spacing: H, V  NB: rows define size
+    bottom.setLayout(new GridLayout(4,4,5,5));
 
     bottom.add(hostLB);
     bottom.add(host);
     bottom.add(portLB);
     bottom.add(port);
+    bottom.add(countLB);
+    bottom.add(count);
+    bottom.add(modeLB);
+    bottom.add(mode);        
     bottom.add(messageLB);
     bottom.add(message);
     bottom.add(openB);
@@ -121,7 +130,6 @@ public class ClientFrame extends JFrame{
 
     setLayout(new BorderLayout(5,5));
     add("North", liste);
-    add("Center", theGUIPanel);
     add("South", bottom);
 
     setVisible(true);
@@ -131,11 +139,21 @@ public class ClientFrame extends JFrame{
   private void readFields()
   {
     int p = Integer.parseInt(port.getText());
-
     simpleClient.setPort(p);
     simpleClient.setHost(host.getText());
   }
-
+  
+  private void initGUIFrame(){
+    int n = refClient.getCount()<2?2:refClient.getCount()+1; 
+    setSize(200*n/2,600);    
+    theGUIPanel = new GUIPanel(refClient);
+    
+    add("Center", theGUIPanel);
+    setVisible(true);
+  
+    super.setTitle(refTitle1+" "+refClient.getSysMode()+".  "+refTitle2);
+  }
+  
   public void close()
   {
     try {
@@ -153,15 +171,32 @@ public class ClientFrame extends JFrame{
   public void open()
   {
     try {
-      System.out.println("clientFrame");
+      System.out.println("clientFrame.open()");
   
       readFields();
-      
       simpleClient.openConnection();
+      simpleClient.checkConnection();
+      if (init == true){
+          int clientCount = Integer.parseInt(count.getText());
+          int sysMode = Integer.parseInt(mode.getText());
+          refClient.setCount(clientCount);
+          refClient.setSysMode(sysMode);
+          
+          //server is remote -- set it up via network
+          interClient.initServer(clientCount, sysMode);
+   
+          refClient.initClient();
+          initGUIFrame();
+          refClient.startClient();
+          liste.add("Mode = "+sysMode+"; Count = "+clientCount);          
+
+          init = false;
+          System.out.println("clientFrame.open()");
+      }
     }
     catch (Exception ex)
     {
-      liste.add(ex.toString());
+      liste.add(ex.toString()+": on open");
       liste.makeVisible(liste.getItemCount()-1);
       liste.setBackground(Color.red);
     }
@@ -186,11 +221,5 @@ public class ClientFrame extends JFrame{
   {
     System.exit(0);
   }
-    //////////////////////////////////
-    // Identify Button[i] as an event source
-    public JToggleButton getEventSource(int i){
-        return theGUIPanel.getEventSource(i);
-    }
-
 }
 

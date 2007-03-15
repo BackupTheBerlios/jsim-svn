0f06d38c0af7866b91d18506ffd27a400e1fe5c0
prowdev <prowdev@a24a35a2-691c-0410-8238-feb538c7d8c7>
2007-03-15 21:09:09 +0000

    Renamed and associated with AppController

---
+++ trunk/appclient/src/appgui/ViewPanel.java
@@ -0,0 +1,90 @@
+// SpinnerSys\src\gui\ViewPanel.java
+// Each ViewPanel provides a parameter panel and the display
+// canvas for its Spinner
+//////////////////////////////////////////////////////////////////////////
+package appgui;
+
+import interfaces.IAppController;
+import javax.swing.*;       // for Timer etc
+import java.awt.*;          // for GridLayout
+import java.awt.event.ActionListener;
+
+
+///////////////////////////////////////////////////////////////////////////
+// Roger Prowse
+// History:
+//		 6 Dec 2001:            Built objects in a hierarchy:
+//		26 Sept 2002: 		Tested OK
+//		27 Nov 2002: v2: 	Issued in gui package
+//               8 Aug 06:              Moved to net_beans
+///////////////////////////////////////////////////////////////////////////
+
+public class ViewPanel extends JPanel{
+    IAppController appController;
+    ParameterPanel theParameterPanel;
+    DisplayCanvas theDisplayCanvas;
+    String name;
+    String id;
+    int mode;
+    ActionListener listener;
+    
+    ViewPanel(int i, IAppController appController){
+        this.appController = appController;
+        name = appController.getName();
+        id = appController.getId();
+        mode = appController.getMode();
+        listener = appController.getListener();
+        ////////////////////////////////////////
+        // Now set up an activity panel,
+        // and setBackground, size and border
+        // Set grid 1 row, 2 columns, spacing 10 horiz, 50 vert
+        setBorder(BorderFactory.createEtchedBorder(Color.white, Color.blue));
+        setLayout(new GridLayout(1,2,20,20));
+        // Instantiate the objects in the activityPanel
+        // and add to the activityPanel
+        theParameterPanel = new ParameterPanel(name, id, mode, listener);
+        add(theParameterPanel);
+        System.out.println("activityPanel: Add parameterPanel " + i);
+        
+        // Build the display canvas and add to the activityPanel
+        theDisplayCanvas = new DisplayCanvas(i, appController);
+        add(theDisplayCanvas);
+        System.out.println("activityPanel: Add display canvas " + i);
+    }
+    
+    public Insets getInsets(){// n,w,s,e
+        return new Insets(20,20,20,20);
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String val) {
+        this.name = val;
+    }
+
+    public String getId() {
+        return id;
+    }
+    public void setId(String val) {
+        this.id = val;
+    }
+
+//    public int getSysMode() {
+//        return sysMode;
+//    }
+//    public void setSysMode(int val) {
+//        this.sysMode = val;
+//    }
+
+    public ActionListener getListener() {
+        return listener;
+    }
+
+    public void setListener(ActionListener val) {
+        this.listener = val;
+    }
+}// ViewPanel
+
+
+

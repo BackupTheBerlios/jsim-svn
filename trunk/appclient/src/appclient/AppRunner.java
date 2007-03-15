752e476b15ff5f4543246b06f4ce5cd14ce08583
prowdev <prowdev@a24a35a2-691c-0410-8238-feb538c7d8c7>
2007-03-15 20:52:28 +0000

    Acts as a container for AppObj, and services calls to update()

--- trunk/appclient/src/appclient/AppRunner.java
+++ trunk/appclient/src/appclient/AppRunner.java
@@ -8,8 +8,7 @@ package appclient;
 
 //import appserver.Record;
 import interfaces.*;
-import java.awt.event.ActionListener;
-import java.util.Observable;
+import spinner.Spinner;
 
 //////////////////////////////////////////////////////////////////////
 // Roger Prowse
@@ -18,93 +17,34 @@ import java.util.Observable;
 //			Separated from Spinners as for appTimers
 //////////////////////////////////////////////////////////////////////
 
-public class AppRunner implements Runnable {
+public class AppRunner implements IAppRunner {
     // Attributes
     IServer refServer;
-    Spinner theSpinner;
-    ButtonSensor theButtonSensor;
-    IStatus status;
-        int id;
-        int mode;
-        int delay;
-        int delayFactor;
-        int period;
-    IRecord record;
-    boolean initRunner;
+    IAppObj appObj;
+    IAppController theAppController;
+    IStatus theStatus;
+    IRecord theRecord;
+    int i;
     
     // Constructor
-    public AppRunner(IServer appServer, int i){
+    public AppRunner(IServer appServer, int i){        
         refServer = appServer;
-        id = i; //then overwritten by getStartupStatus
-        theSpinner = new Spinner(i);
-        theButtonSensor = new ButtonSensor(i);
-//        record = new Record();
-        System.out.println("Made appRunner "+id);
-        initRunner = true;
-    }
-        
-    public void setupRunner(int i){       
-        status = refServer.getStartupStatus(i);
-        System.out.println("AppRunner: setupRunner(): startupStatus "+i+" "+status);               
-        id = status.getId();
-        mode = status.getMode();
-        //Define running speeds for each spinner
-        delay = status.getDelay();
-        delayFactor = status.getDelayFactor();
-        period = delay*delayFactor;
-        record = refServer.getRunningRecord(i);
-        theSpinner.startSpinner(status);
-        System.out.println("initialised appRunner "+id+": " + delay+" * "+delayFactor+" = "+period);       
-    }
-
-    /////////////////////////////////////////////////////////////////
-    public void run(){
-        try{
-            while (true) {
-                // If button has been pressed to stop, then wait
-//                System.out.println("AppRunner: run() "+id);               
-                theButtonSensor.checkGoingState();
-                if (initRunner == true){
-                    setupRunner(id);
-                    initRunner = false;
-                }               
-                if(theSpinner.executeOneStep(record)){
-                    //send the id to the server which controls the parameters
-                    // for a pattern such as a Spinner.
-                    // Tell server to generate a new runningRecord
-//                    IRecord record1 = new Record();
-                    System.out.println("AppRunner: run().cycleEnded "+id);               
-                    record = refServer.cycleEnded(id);
-//                    record3.setRecord(record1);
-//                    record.setRecord(record3);
-                }
-                Thread.sleep(period);
-            }
-        }catch(InterruptedException e){
-            System.out.println("Interrupted sleep");
-        }
+        this.i = i;
+        appObj = appServer.getAppObj(i);
+        theAppController = appObj.getAppController();
+        theStatus = appObj.getStatus();
+        theRecord = appObj.getRecord();
+        theAppController.startController(this, theStatus);
+        System.out.println("Made appRunner "+i);
     }
     
-    public int getMode(){
-        return mode;
+    public IAppController getAppController(){
+        return theAppController;        
     }
-
-    /**
-     * Identifies the spinner as an observable object
-     * @return reference to spinner[i]
-     */
-    public Observable getObservable(){        
-        return theSpinner;
+    
+    public IRecord update(){
+        return refServer.cycleEnded(i);                
     }
-
-    /**
-     * Identifies theButtonSensor as a Listener
-     * @return reference to theButtonSensor[i]
-     */
-    public ActionListener getListener(){        
-        return theButtonSensor;
-    }
-
 }//Spinner
 ///////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////

538f87f52943dcdbe52a6858f385ae273f65a0ff
prowdev <prowdev@a24a35a2-691c-0410-8238-feb538c7d8c7>
2007-03-15 21:23:50 +0000

    Simplified

--- trunk/spinners/appserver/src/appserver/DBManager.java
+++ trunk/spinners/appserver/src/appserver/DBManager.java
@@ -3,33 +3,55 @@ package appserver;
 import interfaces.*;
 public class DBManager {
     
+//    OldDatabase database;
     Database database;
-    
+   
     public DBManager(int clientCount) {
-        database = new Database(clientCount);
+        database = new Database(clientCount);        
+    }
+    
+    public void saveSetup(int i, IAppObj appObj) {
+        database.setUp(i,appObj);
     }
 
-    public IStatus getStatus(int i){
-        return database.getStartupStatuses(i);
-    }
-    public void setStatus(int i, IStatus status){
-        database.setStartupStatuses(i, status);
-    }
+    public IAppModel getAppModel(int i){
+        return database.getAppObj(i).getAppModel();
+    }    
+    public void setAppModel(int i, IAppModel appModel){
+        database.getAppObj(i).setAppModel(appModel);
+    }    
     
-    public void saveSetup(int i, IStatus status, IRecord record) {
-        database.setStartupStatuses(i, status);
-        database.setRunningRecords(i, record);
-    }
+    public IAppController getAppController(int i){
+        return database.getAppObj(i).getAppController();
+    }    
+    public void setAppController(int i, IAppController appController){
+        database.getAppObj(i).setAppController(appController);
+    }    
+    
+    public IStatus getStatus(int i){
+        return database.getAppObj(i).getStatus();
+    }    
+    public void setStatus(int i, IStatus status){
+        database.getAppObj(i).setStatus(status);
+    }    
  
     public IRecord getRecord(int i){
-        return database.getRunningRecords(i);
+        return database.getAppObj(i).getRecord();
     }
     public void setRecord(int i, IRecord record){
-        database.setRunningRecords(i, record);
+        database.getAppObj(i).setRecord(record);
     }
+
+/*
     public Database getDatabase(){
         return database;
     }
-    
+*/    
+    public IAppObj getAppObj(int i){
+        return database.getAppObj(i);
+    }
+    public void setAppObj(int i, IAppObj val){
+        database.setAppObj(i, val);
+    }   
 }
 

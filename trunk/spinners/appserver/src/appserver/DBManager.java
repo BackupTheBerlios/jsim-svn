8bf64b041b41003d3520b075e10ec56d6f0c61a2
prowdev <prowdev@a24a35a2-691c-0410-8238-feb538c7d8c7>
2007-03-22 20:34:46 +0000

    No need to know clientCount.  No need for getController() function in the Server, so removed.

--- trunk/spinners/appserver/src/appserver/DBManager.java
+++ trunk/spinners/appserver/src/appserver/DBManager.java
@@ -3,31 +3,30 @@ package appserver;
 import interfaces.*;
 public class DBManager {
     
-//    OldDatabase database;
     Database database;
    
-    public DBManager(int clientCount) {
-        database = new Database(clientCount);        
+    public DBManager() {
+        database = new Database();        
     }
     
     public void saveSetup(int i, IAppObj appObj) {
         database.setUp(i,appObj);
     }
 
+    public IAppObj getAppObj(int i){
+        return database.getAppObj(i);
+    }
+    public void setAppObj(int i, IAppObj val){
+        database.setAppObj(i, val);
+    }
+    
     public IAppModel getAppModel(int i){
         return database.getAppObj(i).getAppModel();
     }    
     public void setAppModel(int i, IAppModel appModel){
         database.getAppObj(i).setAppModel(appModel);
     }    
-    
-    public IAppController getAppController(int i){
-        return database.getAppObj(i).getAppController();
-    }    
-    public void setAppController(int i, IAppController appController){
-        database.getAppObj(i).setAppController(appController);
-    }    
-    
+        
     public IStatus getStatus(int i){
         return database.getAppObj(i).getStatus();
     }    
@@ -42,16 +41,6 @@ public class DBManager {
         database.getAppObj(i).setRecord(record);
     }
 
-/*
-    public Database getDatabase(){
-        return database;
-    }
-*/    
-    public IAppObj getAppObj(int i){
-        return database.getAppObj(i);
-    }
-    public void setAppObj(int i, IAppObj val){
-        database.setAppObj(i, val);
-    }   
+
 }
 
